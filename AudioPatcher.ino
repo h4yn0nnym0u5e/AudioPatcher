5e7a2989048314e9b1dee3b81b820f2d3aafd69f
Jonathan Oakley <h4yn0nnym0u5e@users.noreply.github.com>
2025-02-24 14:42:03 +0000

    Find SGTL5000 at either address
    git-svn-id: svn://slug1/jonathan/SVNrepo/AudioPatcher/branches/touch-kbd@78 f6964fe3-6982-4f03-a74d-735ce54faba2

--- AudioPatcher.ino
+++ AudioPatcher.ino
@@ -167,9 +167,17 @@ void setup()
 
   printHL();
   Serial.printf("SGTL5000 at %08x\n",(uint32_t) theControlSGTL5000.streamP.ControlSGTL5000);
-  
-  theControlSGTL5000.streamP.ControlSGTL5000->setAddress(HIGH);
-  Serial.printf("SGTL5000 init %s\n",theControlSGTL5000.streamP.ControlSGTL5000->enable()?"success":"failure");
+
+  {
+    uint8_t addr = LOW;
+    bool enabled = theControlSGTL5000.streamP.ControlSGTL5000->enable();
+    if (!enabled)
+    {
+      theControlSGTL5000.streamP.ControlSGTL5000->setAddress(HIGH);
+      enabled = theControlSGTL5000.streamP.ControlSGTL5000->enable();
+    }
+    Serial.printf("SGTL5000 init %s at %s address\n",enabled?"success":"failure", addr == LOW?"low":"high");
+  }
   theControlSGTL5000.streamP.ControlSGTL5000->volume(0.1);
  
   delay(5);
